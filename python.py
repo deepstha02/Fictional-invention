49b899c208074a7d0a6f606a8a27746d901766bb
deepstha02 <shresthadeep172@gmail.com>
2025-11-29 07:38:04 +0545

    day4.py

--- python.py
+++ python.py
@@ -1,2 +1,12 @@
+import tkinter as tk
 
+window = tk.Tk()
+
+tk.Label(window, text="Username").grid(row=0, column=0)
+tk.Entry(window).grid(row=0, column=1)
+
+tk.Label(window, text="Password").grid(row=1, column=0)
+tk.Entry(window, show="*").grid(row=1, column=1)
+
+window.mainloop()``
 
